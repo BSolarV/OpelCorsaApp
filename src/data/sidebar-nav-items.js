08dc4867fbc8de11240e414d451e359c52188a65
BSolar.V <bastian.solar.v@gmail.com>
2020-08-11 23:54:38 -0400

    Icons Added.

--- src/data/sidebar-nav-items.js
+++ src/data/sidebar-nav-items.js
@@ -2,32 +2,32 @@ export default function() {
   return [
     {
       title: "Solicitudes y reservas",
-      htmlBefore: '<i class="material-icons">vertical_split</i>',
+      htmlBefore: '<i class="material-icons">book</i>',
       to: "https://solrec-app.herokuapp.com/",
     },
     {
       title: "Quimioterapia",
       to: "/quimioterapia",
-      htmlBefore: '<i class="material-icons">edit</i>'
+      htmlBefore: '<i class="material-icons">event_seat</i>'
     },
     {
       title: 'Pabellones',
-      htmlBefore: '<i class="material-icons">view_module</i>',
+      htmlBefore: '<i class="material-icons">healing</i>',
       to: 'https://iif-front.herokuapp.com/pabellones/'
     },
     {
       title: 'Salas de recuperación',
-      htmlBefore: '<i class="material-icons">view_module</i>',
+      htmlBefore: '<i class="material-icons">accessible</i>',
       to: 'https://iif-front.herokuapp.com/recuperacion/'
     },
     {
       title: 'Equipamientos',
-      htmlBefore: '<i class="material-icons">view_module</i>',
+      htmlBefore: '<i class="material-icons">biotech</i>',
       to: 'https://sogireact.herokuapp.com/listar-equipamientos/'
     },
     {
       title: 'Personal y Equipos',
-      htmlBefore: '<i class="material-icons">note_add</i>',
+      htmlBefore: '<i class="material-icons">person_search</i>',
       to: 'https://chopinhauer-web.herokuapp.com/all-pservice'
     }
   ];
